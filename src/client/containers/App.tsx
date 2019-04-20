5ada34628855e348cda59f4636847bcced3cc109
ethan <eemandien@mweb.co.za>
2019-04-20 23:11:52 +0200

    -- add react-apollo

--- src/client/containers/App.tsx
+++ src/client/containers/App.tsx
@@ -1,9 +1,36 @@
 import * as React from 'react';
+import gql from 'graphql-tag';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloConsumer } from "react-apollo";
 
-export default () => {
-  return (
-    <div>
-      <button>Initialise App</button>
-    </div>
-  );
+const query = gql`
+  {
+    dota2_team {
+      team_name,
+      tag,
+      wins,
+      losses,
+      logo_url
+    }
+  }
+`;
+
+export default class extends React.Component {
+  constructor(props: any) { super(props); }
+  onInitialiseApp = (client: ApolloClient<any>) => {
+    client.query({ query }).then((result: any) => console.log(result), (err: any) => console.log({ err }));
+  }
+
+  render() {
+    return (
+      <ApolloConsumer>
+        {client => (
+            <div>
+              <button onClick={_ => this.onInitialiseApp(client)}>Initialise App</button>
+            </div>
+          )
+        }
+      </ApolloConsumer>
+    );
+  }
 }
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from 'apollo-boost';
 
 import App from './containers/App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/v1alpha1/graphql',
+  headers: {
+    'content-type': 'application/json',
+    'x-hasura-admin-secret': '"jkasvbdhbjaasdkahbsd"'
+  }
+});
+
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 );
 
