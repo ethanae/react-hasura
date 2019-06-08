7b72f8de06fae2a237daad987319d3941cd00731
ethan <eemandien@mweb.co.za>
2019-06-08 15:41:08 +0200

    - remove commented jsx

--- client/components/Style.tsx
+++ client/components/Style.tsx
@@ -63,26 +63,3 @@ export const Pagination = styled.ul`
     }
   }
 `;
-
-// <ul className="pagination">
-//         {
-//           !props.label ? null :
-//             <li className="page-item" title='pages'>
-//               <a className="page-link">{props.label}</a>
-//             </li>
-//         }
-//         <li className="page-item">
-//           <button
-//             className="page-link"
-//             onClick={props.onPageBackward}>
-//             Previous
-//           </button>
-//         </li>
-//         <li className="page-item">
-//           <button
-//             className="page-link"
-//             onClick={props.onPageForward}>
-//             Next
-//           </button>
-//         </li>
-//       </ul>
