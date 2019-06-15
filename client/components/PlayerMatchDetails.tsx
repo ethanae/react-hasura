def90dba25baf9fe19e9e02db466aafe5b1a9bab
ethan <eemandien@mweb.co.za>
2019-06-15 17:42:19 +0200

    - playermatchdetails layout/styling

--- client/components/PlayerMatchDetails.tsx
+++ client/components/PlayerMatchDetails.tsx
@@ -27,32 +27,35 @@ export default class extends React.Component<Props, { expandDetails: boolean }> 
         onMouseEnter={this.onCardHover}
         onMouseLeave={this.onCardHoverExit}
       >
-                          
-      <div className="flex-row" >
-        <div>
-          <div className="d-flex justify-content-around">
-            <span>{moment(match.start_time * 1000).format('D MMM YYYY')}</span>
-            <span className="text-warning">{match.hero.localized_name}</span>
-            <span className={match.radiant_win ? 'text-success' : 'text-danger'}>
-              {match.radiant_win ? 'Radiant Win' : 'Dire Win'}
-            </span>
-          </div>
-         { this.state.expandDetails &&
-            <div className='d-flex'>
-              <div className="">
-                <p>K: {match.kills}</p>
-                <p>D: {match.deaths}</p>
-                <p>A: {match.assists}</p>
-              </div>
-              <div>
-                <p>LH: {match.last_hits}</p>
-                <p>GPM: {match.gold_per_min}</p>
-                <p>XPM: {match.xp_per_min}</p>
-              </div>
+        <div className="flex-row" >
+          <div>
+            <div className="d-flex justify-content-around">
+              <span>{moment(match.start_time * 1000).format('D MMM YYYY')}</span>
+              <span className="text-warning">{match.hero.localized_name}</span>
+              <span className={match.radiant_win ? 'text-success' : 'text-danger'}>
+                {match.radiant_win ? 'Radiant Win' : 'Dire Win'}
+              </span>
             </div>
-          }
+          { this.state.expandDetails &&
+              <div className="d-flex flex-row justify-content-between">
+                <div className="">
+                  <p title="Kills">K: {match.kills}</p>
+                  <p title="Deaths">D: {match.deaths}</p>
+                  <p title="Assists">A: {match.assists}</p>
+                </div>
+                <div>
+                  <p title="Last hits">LH: {match.last_hits}</p>
+                  <p title="Gold per minute">GPM: {match.gold_per_min}</p>
+                  <p title="XP per minute">XPM: {match.xp_per_min}</p>
+                </div>
+                <div>
+                  <p title="Tower damage">TD: {match.tower_damage}</p>
+                  <p>Duration: {Math.ceil(match.duration / 60)}min</p>
+                </div>
+              </div>
+            }
+          </div>
         </div>
-      </div>
       </CardWrapper>
     );
   }
