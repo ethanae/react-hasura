3be7deb8ea4b81d94f31cfdb88fe468970035d40
ethan <eemandien@mweb.co.za>
2019-07-22 22:10:42 +0200

    - assert rabbit mq channel on subscription

--- message-broker/src/subscribe-topic.ts
+++ message-broker/src/subscribe-topic.ts
@@ -7,9 +7,9 @@ export async function subscribe(exchange: string,
   try {
     const connection = await amqp.connect('amqp://localhost:5672');
     const channel = await connection.createChannel();
-    // await channel.assertExchange(exchange, 'topic', {
-    //   durable: false
-    // });
+    await channel.assertExchange(exchange, 'topic', {
+      durable: false
+    });
 
     const q = await channel.assertQueue('', { exclusive: true });
     await channel.bindQueue(q.queue, exchange, routingKey);
